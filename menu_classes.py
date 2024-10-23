ae6873a24598a1f391efb1ede7d995d83435781f
Gerardo Rios <jose.g.rios@unl.edu.ec>
2024-10-23 15:10:57 -0500

    added defaults, renamed util_unit_test, modified menu_classes

--- menu_classes.py
+++ menu_classes.py
@@ -105,13 +105,3 @@ class Menu:
                 pygame.mixer.music.play()
             else:
                 pygame.mixer.music.pause()
-
-    def update_time(self, seconds, font=None):
-        # Make a sound 1 second before it's time to change players
-        if self.sound == ON and seconds == 1:
-            self.change_player_sound.play()
-        self.time = seconds
-        self.text_objects["time_display"] = TextObject(str(seconds), 
-                                                        *self.positions["time_display"], 
-                                                        font=self.font_time,
-                                                        color=RED)
